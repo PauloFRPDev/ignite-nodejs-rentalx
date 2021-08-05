6747eb27bbf5610151bd3afd04a339c17f764d9d
Paulo Felippe <paulofelipperp.dev@gmail.com>
2021-08-04 23:33:05 -0300

    feature: create rental

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { CreateSpecificationController } from "@modules/cars/useCases/CreateSpecification/CreateSpecificationController";
+import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
